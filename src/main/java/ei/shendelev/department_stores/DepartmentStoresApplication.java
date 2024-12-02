3de022ae0b22edd65c3f717d077e7acad4cf4475
Evgeniy Shendelev <sendeleve@yandex.ru>
2024-12-02 21:56:21 +0300

    Create project

---
+++ src/main/java/ei/shendelev/department_stores/DepartmentStoresApplication.java
@@ -0,0 +1,13 @@
+package ei.shendelev.department_stores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentStoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentStoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ei/shendelev/department_stores/DepartmentStoresApplicationTests.java
@@ -0,0 +1,13 @@
+package ei.shendelev.department_stores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentStoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
